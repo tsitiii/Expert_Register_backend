d593ce68c941a055942918f270eed10d386f71a1
tsitiii <ziontaa9@gmail.com>
2025-05-08 01:19:20 +0300

    allwoed host

--- EDS_backend/settings.py
+++ EDS_backend/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-kaifet71^6jw=106lsd*xgvzg)0r^jbc=(d2nl3%&_yj(zj0!w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
