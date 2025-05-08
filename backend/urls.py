a361f8b26944f53439be1f2a01132851e4472a5d
tsitiii <ziontaa9@gmail.com>
2025-05-08 12:15:16 +0300

    url fixed

--- backend/urls.py
+++ backend/urls.py
@@ -12,11 +12,11 @@ router.register(r'users', UserViewSet, basename='user')
 router.register(r'experts', ExpertViewSet, basename='expert')
 
 urlpatterns = [
-    path('api/v1/', include(router.urls)),
-    path('api/auth/login/', LoginView.as_view(), name='login'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/password_reset/', PasswordResetViewSet.as_view(), name='api_password_reset'),
-    path('api/password_reset/confirm/', PasswordResetConfirmView.as_view(), name='api_password_reset_confirm'),
+    path('v1/', include(router.urls)),
+    path('auth/login/', LoginView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('password_reset/', PasswordResetViewSet.as_view(), name='api_password_reset'),
+    path('password_reset/confirm/', PasswordResetConfirmView.as_view(), name='api_password_reset_confirm'),
     
     # path('api/v1/experts/register/', ExpertViewSet.as_view({'post': 'create'}), name='expert-register'),
     path('api/v1/experts/<int:expert_id>/build-cv/', CVBuilderAPIView.as_view({'post': 'create', 'put': 'update', 'patch': 'partial_update'}), name='build-cv'),
